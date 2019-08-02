0330d6348a6ef0066cb235388e14d2f533b58a12
ammartahir24 <47375983+ammartahir24@users.noreply.github.com>
2019-08-02 20:36:55 +0500

    Update test1.js

--- test-data/orig/test1.js
+++ test-data/orig/test1.js
@@ -12,13 +12,17 @@ var writefile = function(file,data,cb){
 	})
 }
 
-var list_of_files = ['a.txt','b.txt','c.txt']
+var list_of_files = ['a_files_directory.txt','b_files_directory.txt','c_files_directory.txt']
 
 var To_Upper_Case = function(data){
 	return data.toUpperCase()
 }
 
-var readfiles_from_list = function(list){
+var Append_To_Name = function(pre,post){
+	return pre+post
+}
+
+var convert_files = function(list){
 	list.forEach(function (i){
 		readfile(i, function(data){
 			var first_file = data[0]
@@ -27,20 +31,23 @@ var readfiles_from_list = function(list){
 			readfile(first_file, function(d1){
 				readfile(second_file, function(d2){
 					readfile(third_file, function(d3){
-						var n_first = To_Upper_Case(first_file)
-						var n_snd = To_Upper_Case(second_file)
-						var n_thrd = To_Upper_Case(third_file)
-						console.log(n_first,d1)
-						console.log(n_snd,d2)
-						console.log(n_thrd,d3)
-						writefile(first_file,d1,function(e1){
-							console.log(e1)
+						var name_1 = Append_To_Name("1".first_file)
+						var name_2 = Append_To_Name("2".second_file)
+						var name_3 = Append_To_Name("3".third_file)
+						var d1_cap = To_Upper_Case(d1)
+						var d2_cap = To_Upper_Case(d2)
+						var d3_cap = To_Upper_Case(d3)
+						console.log(name_1,d1_cap)
+						console.log(name_2,d2_cap)
+						console.log(name_3,d3_cap)
+						writefile(name_1,d1_cap,function(e){
+							console.log(e)
 						})
-						writefile(second_file,d2,function(e2){
-							console.log(e2)
+						writefile(name_2,d2_cap,function(e){
+							console.log(e)
 						})
-						writefile(third_file,d3,function(e3){
-							console.log(e3)
+						writefile(name_3,d3_cap,function(e){
+							console.log(e)
 						})
 					})
 				})
@@ -49,4 +56,4 @@ var readfiles_from_list = function(list){
 	})
 }
 
-readfiles_from_list(list_of_files);
+convert_files(list_of_files);
